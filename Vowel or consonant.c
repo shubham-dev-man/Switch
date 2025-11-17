8decdbc88215cef340df43d9a1b14773d2108bd1
shubham-dev-man <www.shubhampundir0007@gmail.com>
2025-11-17 12:01:36 +0530

    Create Vowel or consonant.c

---
+++ Vowel or consonant.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    switch(ch) {
+        case 'a': case 'e': case 'i': case 'o': case 'u':
+        case 'A': case 'E': case 'I': case 'O': case 'U':
+            printf("Vowel\n"); break;
+        default:
+            printf("Consonant\n");
+    }
+    return 0;
+}
